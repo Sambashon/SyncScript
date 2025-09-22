79248561b05aac6f6561d8dbe978c58917fc8504
Sambashon <sebastianafuenmayord@gmail.com>
2025-09-22 16:38:37 -0300

    Example config.sh added

---
+++ scripts/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#THIS IS WHERE I CONFIGURE ALL MY DIRECTORIES TO SYNC
+
+DESKTOP_IP='first device IP'
+LAPTOP_IP='second devices ip'
+
+DESKTOP_USER='first device user'
+LAPTOP_USER='second device user'
+
+echo 'Source working properly'
